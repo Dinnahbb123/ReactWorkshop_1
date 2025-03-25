9bd4eb278511567909254b1618169b7b69bb562a
Dinnahbb123 <bongoman56@gmail.com>
2025-03-24 22:30:21 -0500

    Add files via upload

---
+++ CampsitesList.js
@@ -0,0 +1,18 @@
+import { Row, Col } from 'reactstrap';
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import CampSiteCard from './CampSiteCard';
+
+const CampsitesList = () => {
+    return(<Row className ='ms-auto'>
+    {CAMPSITES.map((campsite) => {
+        return ( 
+        <Col md='5' className='m-4' key={campsite.id}> 
+            <CampSiteCard campsite={campsite} />
+        </Col>
+        );
+    })}
+    </Row>
+    )
+}
+
+export default CampsitesList;
